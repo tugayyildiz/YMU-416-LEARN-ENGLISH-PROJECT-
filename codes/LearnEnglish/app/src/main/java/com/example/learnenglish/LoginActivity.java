0882fa70e432610b192385f8a5f578b9c70dc265
Erhan <76782523+ErhanKocaman@users.noreply.github.com>
2021-05-29 21:36:38 +0300

    Case-2-F1 Kullanıcı Adı Şifre Giriş Ekranı Mimari Yapı Oluşturuldu

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/LoginActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/LoginActivity.java
@@ -2,13 +2,46 @@ package com.example.learnenglish;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
-
+    EditText emailTextLogin, passwordTextLogin;
+    String email,password;
+    Button login;
+    TextView register;
+    TextInputLayout emailError, passError;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+    private void initView() {
+        emailTextLogin = (EditText) findViewById(R.id.email);
+        passwordTextLogin = (EditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        register = (TextView) findViewById(R.id.register);
+        emailError = (TextInputLayout) findViewById(R.id.emailError);
+        passError = (TextInputLayout) findViewById(R.id.passError);
     }
 }
+
