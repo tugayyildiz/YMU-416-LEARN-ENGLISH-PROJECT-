5c6588e837dc255b133b7ef497d72503051c5f3b
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-05-30 21:24:12 +0300

    feat: CASE-2-F1 Kullanıcı Adı-Şifre Giriş Ekranı için veritabanı için user id modeli oluşturuldu.

---
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/UserId.java
@@ -0,0 +1,13 @@
+package com.example.learnenglish;
+
+public class UserId {
+    public String userid;
+
+    public UserId(){
+
+    }
+
+    public UserId(String userid){
+        this.userid=userid;
+    }
+}
