22377914edf3e97323fe2f0b5dabc433216a15a0
Erhan <76782523+ErhanKocaman@users.noreply.github.com>
2021-05-30 21:39:13 +0300

    Case-2-F1 Kullanıcı Adı Şifre Giriş Ekranı  Login İşlemi Tamamlandı

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/LoginActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/LoginActivity.java
@@ -1,15 +1,22 @@
 package com.example.learnenglish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     EditText emailTextLogin, passwordTextLogin;
@@ -17,6 +24,8 @@ public class LoginActivity extends AppCompatActivity {
     Button login;
     TextView register;
     TextInputLayout emailError, passError;
+    boolean isEmailValid, isPasswordValid;
+    private FirebaseAuth loginAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,13 +34,15 @@ public class LoginActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+		dologin();
             }
         });
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+		Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -42,6 +53,29 @@ public class LoginActivity extends AppCompatActivity {
         register = (TextView) findViewById(R.id.register);
         emailError = (TextInputLayout) findViewById(R.id.emailError);
         passError = (TextInputLayout) findViewById(R.id.passError);
+    }
+private void dologin() {
+        loginAuth = FirebaseAuth.getInstance();
+        email=emailTextLogin.getText().toString();
+        password=passwordTextLogin.getText().toString();
+
+        if(!(email.isEmpty())&&!(password.isEmpty())){
+            loginAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        System.out.println("Giriş Başarılı");
+                        Toast.makeText(LoginActivity.this,"Giriş Başarılı",Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }else{
+                        System.out.println("Giriş Başarısız");
+                        Toast.makeText(LoginActivity.this,"Giriş Başarısız",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
     }
 }
 
