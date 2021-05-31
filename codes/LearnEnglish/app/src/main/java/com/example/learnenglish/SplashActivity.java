a431455ffe73b6ad303f897dab90afcf09f596de
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-05-31 23:35:24 +0300

    feat:Case-1-F1 Hoşgeldiniz Ekranı Tasarımı kodları tamamlandı.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/SplashActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/SplashActivity.java
@@ -1,14 +1,26 @@
 package com.example.learnenglish;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
-
+    private int nextTime = 2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent next = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(next);
+                finish();
+            }
+        },nextTime);
     }
 }
