7b0bf13751952e25c2484866506a404a39a168ba
Erhan <76782523+ErhanKocaman@users.noreply.github.com>
2021-06-01 23:36:08 +0300

    Case-4-F1 Kayıt Ekranı Database Bağlantısı Tamamlandı.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/RegisterActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/RegisterActivity.java
@@ -10,7 +10,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -19,7 +24,11 @@ public class RegisterActivity extends AppCompatActivity {
     String password;
     TextView login;
     User user;
-
+    private FirebaseAuth registerAuth;
+    private DatabaseReference ref;
+    private FirebaseDatabase database;
+    private String userUid;
+    private int score=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,9 +37,56 @@ public class RegisterActivity extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+		doRegister();
             }
         });
+    }
+	private void doRegister() {
+        registerAuth=FirebaseAuth.getInstance();
+       String email= emailTextRegister.getText().toString();
+        password = passwordTextRegister.getText().toString();
+       String name = nameTextRegister.getText().toString();
+       if (!(email.isEmpty())&&!(password.isEmpty())&&!(name.isEmpty())){
+           registerAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if (task.isSuccessful()){
+                       System.out.println("Kullanıcı oluşturuldu..");
+                        Toast.makeText(RegisterActivity.this,"Kullanıcı oluşturuldu...",Toast.LENGTH_LONG).show();
+                        saveUserDatabase();
+                       Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                       startActivity(intent);
+                       finish();
+                   }else{
+                       System.out.println("Kullanıcı oluşturulamadı..");
+                       Toast.makeText(RegisterActivity.this,"Kullanıcı oluşturulamadı...",Toast.LENGTH_LONG).show();
+                   }
+               }
+           });
+           }
+       }
+	private void saveUserDatabase() {
+        userUid = registerAuth.getUid();
+        database=FirebaseDatabase.getInstance();
+        ref=database.getReference();
+        if (userUid!=null){
+             user = new User(emailTextRegister.getText().toString(),nameTextRegister.getText().toString(),password,score);
+            ref.child("users").child(userUid).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful()){
+                        System.out.println("Kullanıcı Database kaydedildi..");}
+                    else{
+                        System.out.println("error...");
+                    }
+                }
+            });
+        }else{
+            System.out.println("Userid has no useruid... ");
+        }
+    }
+	public String userReturn(){
+        return  userUid;
     }
     private void initView() {
         emailTextRegister= ((EditText) findViewById(R.id.email));
