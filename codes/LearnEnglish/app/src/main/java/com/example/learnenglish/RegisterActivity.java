60b054967ba32ca28eddae375ffe47d157a49347
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-06-01 23:20:14 +0300

    feat:Case-4-F1 Kayıt Ekranı Arkaplan Kodları Oluşturuldu.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/RegisterActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/RegisterActivity.java
@@ -1,14 +1,42 @@
 package com.example.learnenglish;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText emailTextRegister,nameTextRegister,passwordTextRegister;
+    Button register;
+    String password;
+    TextView login;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        initView();
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+    private void initView() {
+        emailTextRegister= ((EditText) findViewById(R.id.email));
+        nameTextRegister= ((EditText) findViewById(R.id.name));
+        passwordTextRegister= ((EditText) findViewById(R.id.password));
+        register= ((Button) findViewById(R.id.register));
+        login= ((TextView) findViewById(R.id.login));
     }
 }
