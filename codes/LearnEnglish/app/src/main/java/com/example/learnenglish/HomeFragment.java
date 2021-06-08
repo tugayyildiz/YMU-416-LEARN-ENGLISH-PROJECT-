bfa46c27f2786ab5c4b29aa9689ef5fb431eb28e
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-06-08 23:07:33 +0300

    feat:Case-22-F4 Translate Sayfası ArkaPlan Kodları Oluşturuldu.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/HomeFragment.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/HomeFragment.java
@@ -1,64 +1,92 @@
-package com.example.learnenglish;
+package com.example.learnenglish.;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.learnenglish.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Text;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
 public class HomeFragment extends Fragment {
+    EditText editText;
+    TextView previousText, translatedText;
+    Button firstLanguageButton, secondLanguageButton;
+    Switch switchButton;
+    ImageButton translateButton, favButton;
+    String text, firstLanguage, secondLanguage;
+    private String useruid1;
+    private FirebaseAuth registerAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    private String userUid;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        firstLanguageButton = ((Button) v.findViewById(R.id.button));
+        secondLanguageButton = ((Button) v.findViewById(R.id.button2));
+        switchButton = ((Switch) v.findViewById(R.id.switch1));
+        translateButton = ((ImageButton) v.findViewById(R.id.translateButton));
+        editText = ((EditText) v.findViewById(R.id.translateText));
+        favButton = ((ImageButton) v.findViewById(R.id.favButton));
+        translatedText = ((TextView) v.findViewById(R.id.translatedText));
+        previousText = ((TextView) v.findViewById(R.id.previousText));
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            private String resultText;
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
-}
+            @Override
+            public void onClick(View view) {
+                text = editText.getText().toString();
+                firstLanguage = firstLanguageButton.getText().toString().toUpperCase();
+                secondLanguage = secondLanguageButton.getText().toString().toUpperCase();
+                System.out.println(text);
+                ((MainActivity) getActivity()).translateText(firstLanguage, secondLanguage, text);
+                System.out.println(resultText);
+                final Timer timer = new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+
+                    @Override
+                    public void run() {
+                        if (((MainActivity) getActivity()).resultText != null) {
+                            previousText.setText(text);
+                            translatedText.setText(((MainActivity) getActivity()).resultText);
+                            editText.setText("");
+                            editText.setHint("Buraya yazınız");
+                            ((MainActivity) getActivity()).resultText = null;
+                            timer.cancel();
+
+                        }
+                    }
+                }, 0, 100);
+            }
+        });
+
+        switchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String temporaryText = (String) secondLanguageButton.getText();
+                secondLanguageButton.setText(firstLanguageButton.getText());
+                firstLanguageButton.setText(temporaryText);
+            }
+        });
+    }}
--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public char[] resultText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void translateText(String firstLanguage, String secondLanguage, String text) {
     }
 }
