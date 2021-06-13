cfe2b91f3fe662cc17f04e9112104a1f45111382
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-06-13 23:44:29 +0300

    feat: main.activity düzenlemeler yapıldı.

---
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/LearnFragment.java
@@ -0,0 +1,6 @@
+package com.example.learnenglish;
+
+import androidx.fragment.app.Fragment;
+
+public class LearnFragment extends Fragment {
+}
--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
@@ -1,19 +1,85 @@
 package com.example.learnenglish;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    public char[] resultText;
-
+    private String language;
+    EditText editText ;
+    Button firstLanguageButton,secondLanguageButton;
+    String firstLanguage,secondLanguage,resultText;
+    TextView  translatedText,previousText;
+    int a;
+    RadioButton radioButton;
+    RadioGroup radioGroup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        amplifyConfig();
+        initBottomNavView();
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new HomeFragment()).commit();
     }
 
-    public void translateText(String firstLanguage, String secondLanguage, String text) {
+    private void amplifyConfig() {
     }
-}
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()){
+                case R.id.nav_home:
+                    selectedFragment=new HomeFragment();
+                    break;
+                case R.id.nav_learn:
+                    selectedFragment =new LearnFragment();
+                    break;
+                case R.id.nav_words:
+                    selectedFragment =new WordsFragment();
+                    break;
+                case R.id.nav_saved:
+                    selectedFragment=new SaveFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,selectedFragment).commit();
+            return true;
+        }
+    };
+
+    private void initBottomNavView() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navlistener);
+    }
+
+    public void alert(int b){
+        a=b;
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.languages,null);
+        radioGroup = view.findViewById(R.id.languages);
+        Button buttonApply = view.findViewById(R.id.button_apply);
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setView(view);
+        final AlertDialog alertDialog = alert.create();
+        buttonApply.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View view) {
+
+                translatedText= ((TextView) view.findViewById(R.id.translatedText));
+                previousText= ((TextView) view.findViewById(R.id.previousText));
