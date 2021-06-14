9e03547f32a8f363369fb60147415b7270e98e45
Yusuf EFE <65026569+Yusuf-E@users.noreply.github.com>
2021-06-14 19:06:47 +0300

    feat : Kelimeler Arkaplan Ekranı Tamamlandı.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/WordsFragment.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/WordsFragment.java
@@ -1,64 +1,84 @@
 package com.example.learnenglish;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WordsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.xwray.groupie.GroupAdapter;
+import com.xwray.groupie.GroupieViewHolder;
+
+import java.util.ArrayList;
+
 public class WordsFragment extends Fragment {
+    private RecyclerView recylerView;
+    private GroupAdapter adapter;
+    private FirebaseUser user;
+    private FirebaseAuth registerAuth;
+    private FirebaseDatabase database;
+    private DatabaseReference ref;
+    private String userUid;
+    int queue=0;
+    private Iterable<DataSnapshot> user1;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+        View v= inflater.inflate(R.layout.fragment_words,container,false);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public WordsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WordsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WordsFragment newInstance(String param1, String param2) {
-        WordsFragment fragment = new WordsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return v;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recylerView = ((RecyclerView) view.findViewById(R.id.recyclerView));
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(view.getContext());
+        linearLayoutManager.setReverseLayout(true);
+        linearLayoutManager.setStackFromEnd(true);
+        recylerView.setLayoutManager(linearLayoutManager);
+        //recylerView.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        adapter = new GroupAdapter<GroupieViewHolder>();
+        recylerView.setAdapter(adapter);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_words, container, false);
+        database= FirebaseDatabase.getInstance();
+        ref=database.getReference().child("users");
+        Query query = ref.orderByChild("score");
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                queue= (int) snapshot.getChildrenCount();
+                for (DataSnapshot child : snapshot.getChildren()) {
+
+                    System.out.println((child.child("score").getValue()));
+                    System.out.println(child.child("username").getValue().toString());
+                    if ((child.child("username").getValue().toString())!=null){
+                        adapter.add(new Score(queue,child.child("username").getValue().toString(),child.child("score").getValue().toString()));
+                        queue--;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
