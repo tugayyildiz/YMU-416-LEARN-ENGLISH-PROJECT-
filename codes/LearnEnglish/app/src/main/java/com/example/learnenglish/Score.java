fa9363636dd72ee3270bef9aff4c76c5422a5ac1
Tugay YILDIZ <76121438+tugayyildiz@users.noreply.github.com>
2021-06-14 22:18:02 +0300

    feat: Skor hesaplama işlemlerinin yapılması.

---
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/Score.java
@@ -0,0 +1,41 @@
+package com.example.learnenglish;
+
+import android.content.ClipData;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.xwray.groupie.GroupieViewHolder;
+import com.xwray.groupie.Item;
+
+public class Score extends ClipData.Item {
+    private  String queue;
+    private  String name,score;
+    private TextView queueView,nameView,scoreView;
+    public  Score(){
+        super();
+
+    }
+    public Score(int queue,String name,String score) {
+        this.queue=String.valueOf(queue);
+        this.name=name;
+        this.score = score;
+    }
+    @Override
+    public void bind(@NonNull GroupieViewHolder viewHolder, int position) {
+        queueView=viewHolder.itemView.findViewById(R.id.queue);
+        nameView=viewHolder.itemView.findViewById(R.id.name);
+        scoreView=viewHolder.itemView.findViewById(R.id.score);
+        queueView.setText(queue);
+        nameView.setText(this.name);
+        scoreView.setText(this.score);
+        queueView.setText(this.queue);
+
+    }
+
+    @Override
+    public int getLayout() {
+        return R.layout.score_layout;
+    }
+}
