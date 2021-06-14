67d925468c3e0289b61f749458365756f2fb25c8
Yusuf EFE <65026569+Yusuf-E@users.noreply.github.com>
2021-06-14 18:51:29 +0300

    feat : MainActivity Güncellendi.

--- codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
+++ codes/LearnEnglish/app/src/main/java/com/example/learnenglish/MainActivity.java
@@ -1,21 +1,39 @@
 package com.example.learnenglish;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.auth.cognito.AWSCognitoAuthPlugin;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.predictions.aws.AWSPredictionsPlugin;
+import com.amplifyframework.predictions.models.LanguageType;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.hitomi.cmlibrary.CircleMenu;
+import com.hitomi.cmlibrary.OnMenuSelectedListener;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionButton;
+import com.oguzdev.circularfloatingactionmenu.library.FloatingActionMenu;
+import com.oguzdev.circularfloatingactionmenu.library.SubActionButton;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private String language;
@@ -33,9 +51,6 @@ public class MainActivity extends AppCompatActivity {
         amplifyConfig();
         initBottomNavView();
         getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new HomeFragment()).commit();
-    }
-
-    private void amplifyConfig() {
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navlistener = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -83,3 +98,61 @@ public class MainActivity extends AppCompatActivity {
 
                 translatedText= ((TextView) view.findViewById(R.id.translatedText));
                 previousText= ((TextView) view.findViewById(R.id.previousText));
+                editText = ((EditText) view.findViewById(R.id.translateText));
+                if (a == 1&&radioButton!=null){
+                    firstLanguageButton=findViewById(R.id.button);
+                    firstLanguage=radioButton.getText().toString();
+                    firstLanguageButton.setText(firstLanguage);
+                }
+                else {
+                    if (radioButton!=null&&a==0){
+                        secondLanguageButton=findViewById(R.id.button2);
+                        secondLanguage = radioButton.getText().toString();
+                        secondLanguageButton.setText(secondLanguage);
+                    }
+
+                }
+                alertDialog.cancel();
+            }
+        });
+
+        alertDialog.show();
+    }
+
+
+    public void checkButton(View view) {
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton=view.findViewById(radioId);
+        System.out.println(radioButton.getText());
+    }
+    private void amplifyConfig(){
+        try {
+            Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSPredictionsPlugin());
+
+            Amplify.configure(getApplicationContext());
+            Log.i("MyAmplifyApp", "Initialized Amplify");
+        } catch (AmplifyException error) {
+            Log.e("MyAmplifyApp", "Could not initialize Amplify", error);
+        }
+    }
+    public void translateText(String firstLanguage , String secondLanguage,String text){
+        System.out.println(text);
+        Amplify.Predictions.translateText(
+                text,
+                LanguageType.valueOf(firstLanguage),
+                LanguageType.valueOf(secondLanguage),
+                result ->{
+                    Log.i("MyAmplifyApp", result.getTranslatedText());
+                    resultText=result.getTranslatedText();
+
+
+                },
+                error -> Log.e("MyAmplifyApp", "Translation failed", error)
+
+
+        );
+
+
+    }
+}
